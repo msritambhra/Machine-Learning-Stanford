7bae8b387ff6f639d8e9944f51ab9e3dd717f690
msritambhra <covertlyaphrodite@gmail.com>
2019-08-18 01:46:24 +0530

    ex1 octave

---
+++ Linear Regression and Gradient Descent ( with Multivariable)/ex1/plotData.m
@@ -0,0 +1,13 @@
+function plotData(x, y)
+
+
+figure; % open a new figure window
+plot(x,y,'rx','MarkerSize',10);
+xlabel('Population of City in 10,000s');
+ylabel('Revenuein $10,000s');
+
+
+
+% ============================================================
+
+end
